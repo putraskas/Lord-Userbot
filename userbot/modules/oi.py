1bfd9c15ed22006ebd8e0097bb83d656ca6fd756
putraskas <81615796+putraskas@users.noreply.github.com>
2021-04-27 20:24:09 +0700

    Update oi.py

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -8,7 +8,7 @@ async def typewriter(typew):
     sleep(3)
     await typew.edit("`Hai Perkenalkan Namaku Kulin`")
     sleep(3)
-    await typew.edit("`21 Tahun`")
+    await typew.edit("`19 Tahun`")
     sleep(1)
     await typew.edit("`Tinggal Dibandung, Salam Kenal`")
 # Create by myself @localheart
